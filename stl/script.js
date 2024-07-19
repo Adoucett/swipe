449378568b57a5a37a40f6b95570d188eea9853d
Aaron Doucett <aaron@doucett.net>
2024-07-19 14:27:23 -0500

    update

--- stl/script.js
+++ stl/script.js
@@ -1,182 +1,152 @@
-const container = document.getElementById('image-compare-container');
-const slider = document.getElementById('slider');
-const imgA = document.getElementById('imgA');
-const imgB = document.getElementById('imgB');
-const selectA = document.getElementById('selectA');
-const selectB = document.getElementById('selectB');
-const dataset = document.getElementById('dataset');
-const zoomInBtn = document.getElementById('zoom-in');
-const zoomOutBtn = document.getElementById('zoom-out');
-const resetViewBtn = document.getElementById('reset-view');
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWRvdWNldHQiLCJhIjoiY2lvZDFsc2lwMDRnd3Zha2pneWpxcHh6biJ9.sbWgw2zPGyScsp-r4CYQnA';
+
+const datasets = {
+    'FP': {
+        coordinates: [
+            [-90.27096005, 38.63411217],
+            [-90.2431005, 38.6303475],
+            [-90.2470013, 38.6192146],
+            [-90.27227390, 38.62250509]
+        ],
+        center: [-90.258442, 38.626422]
+    },
+    'CWE': { 
+        coordinates: [
+            [-90.26590405, 38.64820128],
+            [-90.2309325, 38.6409926],
+            [-90.2710217, 38.6319911],
+            [-90.2365457, 38.6249927]
+        ],
+        center: [-90.250721, 38.636425]  //38.636425,-90.250721
+    },
+    'BH': { 
+        coordinates: [
+            [-90.26590405, 38.64820128],
+            [-90.2309325, 38.6409926],
+            [-90.2710217, 38.6319911],
+            [-90.2365457, 38.6249927]
+        ],
+        center: [-90.250721, 38.636425]
+    },
+    'North': { 
+        coordinates: [
+            [-90.26590405, 38.64820128],
+            [-90.2309325, 38.6409926],
+            [-90.2710217, 38.6319911],
+            [-90.2365457, 38.6249927]
+        ],
+        center: [-90.250721, 38.636425]
+    },
+    'BR': { 
+        coordinates: [
+            [-90.26590405, 38.64820128],
+            [-90.2309325, 38.6409926],
+            [-90.2710217, 38.6319911],
+            [-90.2365457, 38.6249927]
+        ],
+        center: [-90.250721, 38.636425]
+    },
+    'DT': { 
+        coordinates: [
+            [-90.26590405, 38.64820128],
+            [-90.2309325, 38.6409926],
+            [-90.2710217, 38.6319911],
+            [-90.2365457, 38.6249927]
+        ],
+        center: [-90.250721, 38.636425]
+    }
+};
 
 const years = [2002, 2004, 2006, 2008, 2010, 2012, 2014, 2016, 2018, 2020, 2022, 2024];
-let startX = 0;
-let active = false;
-let zoomLevel = 1;
-let sliderPosition = 0.5;
-let panX = 0;
-let panY = 0;
 
 const updateDropdowns = (dataset) => {
     const options = years.map(year => `<option value="${dataset}_${year}.jpg">${year}</option>`).join('');
-    selectA.innerHTML = options;
-    selectB.innerHTML = years.map(year => `<option value="${dataset}_${year}.jpg" ${year === 2024 ? 'selected' : ''}>${year}</option>`).join('');
-    imgA.src = `${dataset}_2002.jpg`;
-    imgB.src = `${dataset}_2024.jpg`;
+    document.getElementById('selectA').innerHTML = options;
+    document.getElementById('selectB').innerHTML = years.map(year => `<option value="${dataset}_${year}.jpg" ${year === 2024 ? 'selected' : ''}>${year}</option>`).join('');
 };
 
-const setSliderPosition = (x) => {
-    x = Math.max(0, Math.min(x, container.offsetWidth - slider.offsetWidth));
-    sliderPosition = x / (container.offsetWidth - slider.offsetWidth);
-    slider.style.left = `${x}px`;
-    updateClip();
-};
+const updateMap = (map, dataset, year) => {
+    const sourceId = `${map.getContainer().id}-overlay`;
+    const imgUrl = `https://adoucett.github.io/swipe/stl/${dataset}_${year}.jpg`;
+    const coordinates = datasets[dataset].coordinates;
 
-const onDrag = (e) => {
-    if (!active) return;
-    requestAnimationFrame(() => {
-        const x = (e.clientX || e.touches[0].clientX) - startX;
-        setSliderPosition(x);
-    });
-};
-
-const onStopDrag = () => {
-    active = false;
-    document.removeEventListener('mousemove', onDrag);
-    document.removeEventListener('mouseup', onStopDrag);
-    document.removeEventListener('touchmove', onDrag);
-    document.removeEventListener('touchend', onStopDrag);
-};
-
-const zoom = (clientX, clientY, delta) => {
-    requestAnimationFrame(() => {
-        const rect = container.getBoundingClientRect();
-        const offsetX = (clientX - rect.left - panX) / zoomLevel;
-        const offsetY = (clientY - rect.top - panY) / zoomLevel;
-
-        zoomLevel = Math.min(Math.max(1, zoomLevel + delta), 5);
-
-        panX = clientX - rect.left - offsetX * zoomLevel;
-        panY = clientY - rect.top - offsetY * zoomLevel;
-
-        updateTransform();
-        updateClip();
-    });
-};
-
-const onPan = (e) => {
-    if (!active) return;
-    requestAnimationFrame(() => {
-        panX = (e.clientX || e.touches[0].clientX) - startX;
-        panY = (e.clientY || e.touches[0].clientY) - startY;
-        updateTransform();
-    });
-};
-
-const onStopPan = () => {
-    active = false;
-    document.removeEventListener('mousemove', onPan);
-    document.removeEventListener('mouseup', onStopPan);
-    document.removeEventListener('touchmove', onPan);
-    document.removeEventListener('touchend', onStopPan);
-};
-
-const updateTransform = () => {
-    const transform = `scale(${zoomLevel}) translate(${panX / zoomLevel}px, ${panY / zoomLevel}px)`;
-    imgA.style.transform = transform;
-    imgB.style.transform = transform;
-};
-
-const updateClip = () => {
-    const rect = container.getBoundingClientRect();
-    const clipWidth = sliderPosition * rect.width;
-    imgA.style.clip = `rect(0, ${clipWidth}px, ${rect.height}px, 0)`;
-};
-
-const resetView = () => {
-    zoomLevel = 1;
-    panX = 0;
-    panY = 0;
-    sliderPosition = 0.5;
-    slider.style.left = '50%';
-    updateTransform();
-    updateClip();
-};
-
-const debounce = (func, wait) => {
-    let timeout;
-    return function(...args) {
-        clearTimeout(timeout);
-        timeout = setTimeout(() => func.apply(this, args), wait);
-    };
-};
-
-// Event Listeners
-slider.addEventListener('mousedown', (e) => {
-    active = true;
-    startX = e.clientX - slider.offsetLeft;
-    document.addEventListener('mousemove', onDrag);
-    document.addEventListener('mouseup', onStopDrag);
-});
-
-slider.addEventListener('touchstart', (e) => {
-    active = true;
-    startX = e.touches[0].clientX - slider.offsetLeft;
-    document.addEventListener('touchmove', onDrag);
-    document.addEventListener('touchend', onStopDrag);
-});
-
-selectA.addEventListener('change', (e) => {
-    imgA.src = e.target.value;
-});
-
-selectB.addEventListener('change', (e) => {
-    imgB.src = e.target.value;
-});
-
-dataset.addEventListener('change', (e) => {
-    updateDropdowns(e.target.value);
-});
-
-container.addEventListener('wheel', debounce((e) => {
-    e.preventDefault();
-    zoom(e.clientX, e.clientY, e.deltaY * -0.01);
-}, 100), { passive: true });
-
-container.addEventListener('touchmove', debounce((e) => {
-    e.preventDefault();
-    const touch = e.touches[0];
-    zoom(touch.clientX, touch.clientY, touch.radiusX * -0.01);
-}, 100), { passive: true });
-
-zoomInBtn.addEventListener('click', () => {
-    zoom(container.offsetWidth / 2, container.offsetHeight / 2, 0.1);
-});
-
-zoomOutBtn.addEventListener('click', () => {
-    zoom(container.offsetWidth / 2, container.offsetHeight / 2, -0.1);
-});
-
-resetViewBtn.addEventListener('click', resetView);
-
-container.addEventListener('mousedown', (e) => {
-    if (e.target !== slider) {
-        active = true;
-        startX = e.clientX - panX;
-        document.addEventListener('mousemove', onPan);
-        document.addEventListener('mouseup', onStopPan);
+    if (map.getSource(sourceId)) {
+        map.getSource(sourceId).updateImage({ url: imgUrl, coordinates });
+    } else {
+        map.addSource(sourceId, {
+            type: 'image',
+            url: imgUrl,
+            coordinates: coordinates
+        });
+        map.addLayer({
+            id: sourceId,
+            source: sourceId,
+            type: 'raster',
+            paint: { 'raster-opacity': 1 }
+        });
     }
+};
+
+const recenterMap = (map, center) => {
+    map.flyTo({
+        center: center,
+        zoom: 12
+    });
+};
+
+const datasetSelect = document.getElementById('dataset');
+const selectA = document.getElementById('selectA');
+const selectB = document.getElementById('selectB');
+
+datasetSelect.addEventListener('change', () => {
+    const dataset = datasetSelect.value;
+    updateDropdowns(dataset);
+    updateMap(beforeMap, dataset, selectA.value.split('_')[1].split('.')[0]);
+    updateMap(afterMap, dataset, selectB.value.split('_')[1].split('.')[0]);
+    recenterMap(beforeMap, datasets[dataset].center);
+    recenterMap(afterMap, datasets[dataset].center);
 });
 
-container.addEventListener('touchstart', (e) => {
-    if (e.target !== slider) {
-        active = true;
-        startX = e.touches[0].clientX - panX;
-        startY = e.touches[0].clientY - panY;
-        document.addEventListener('touchmove', onPan);
-        document.addEventListener('touchend', onStopPan);
-    }
+selectA.addEventListener('change', () => {
+    const dataset = datasetSelect.value;
+    updateMap(beforeMap, dataset, selectA.value.split('_')[1].split('.')[0]);
 });
 
-updateDropdowns('FP');
-updateClip();
+selectB.addEventListener('change', () => {
+    const dataset = datasetSelect.value;
+    updateMap(afterMap, dataset, selectB.value.split('_')[1].split('.')[0]);
+});
+
+const beforeMap = new mapboxgl.Map({
+    container: 'before',
+    style: 'mapbox://styles/adoucett/clysuaj18003u01paax2cd5da',
+    center: [-90.258442, 38.626422],
+    zoom: 12
+});
+
+const afterMap = new mapboxgl.Map({
+    container: 'after',
+    style: 'mapbox://styles/adoucett/clysuaj18003u01paax2cd5da',
+    center: [-90.258442, 38.626422],
+    zoom: 12
+});
+
+beforeMap.on('load', () => {
+    const dataset = datasetSelect.value;
+    updateMap(beforeMap, dataset, selectA.value.split('_')[1].split('.')[0]);
+});
+
+afterMap.on('load', () => {
+    const dataset = datasetSelect.value;
+    updateMap(afterMap, dataset, selectB.value.split('_')[1].split('.')[0]);
+});
+
+const container = '#comparison-container';
+const map = new mapboxgl.Compare(beforeMap, afterMap, container, {
+    // Set this to enable comparing two maps by mouse movement:
+    // mousemove: true
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    updateDropdowns(datasetSelect.value);
+});
